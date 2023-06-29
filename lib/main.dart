82613444da93f843961ea0d276fe71108fcef8e7
mojtaba mohammadi <52585886+mojtaba2021@users.noreply.github.com>
2023-06-29 08:37:40 +0430

    create MyApp class

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:tasko/screens/home.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "my fisrt app",
-      home: Scaffold(
-        appBar: AppBar(),
-        body: const Column(
-          children: [
-            Center(
-              child: Text("دراک وب اپ",style: TextStyle(fontFamily: "Vazir")),
-            )
-          ],
-        ),
-      ),
-    ),
-  );
+      theme: ThemeData(fontFamily: "Vazir"),
+      home:  HomeScreen()
+    );
+  }
 }
