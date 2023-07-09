c999ec1e24a07a2fec5c5cec63531c9dcd20bb23
mojtaba mohammadi <52585886+mojtaba2021@users.noreply.github.com>
2023-07-09 11:08:25 +0430

    Refactor homescreen by using custom widgets

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:tasko/screens/task.dart';
+import 'package:tasko/widgets/task/addTaskButton.dart';
+import 'package:tasko/widgets/task/heroSection.dart';
+import 'package:tasko/widgets/task/taskItem.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -40,95 +42,17 @@ class HomeScreenState extends State<HomeScreen> {
                         )
                       ],
                     ),
-                    Container(
-                      margin: EdgeInsets.symmetric(vertical: 10),
-                      padding: EdgeInsets.all(10),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Padding(
-                            padding: EdgeInsets.only(bottom: 10),
-                            child: Text(
-                              "کارها را مدیریت کن",
-                              style: TextStyle(
-                                  fontSize: 16, fontWeight: FontWeight.bold),
-                            ),
-                          ),
-                          Text(
-                            " با اپ تسکو کار های شخصی خودت را مثل آب خوردن مدیرت کن :)",
-                            style: TextStyle(color: Colors.grey[600]),
-                          )
-                        ],
-                      ),
-                    ),
+                    HeroSection(),
                     Expanded(
                       child: ListView.builder(
                         itemCount: 20,
-                        itemBuilder: (BuildContext context, int index) {
-                          return Container(
-                            padding: EdgeInsets.all(10),
-                            margin: EdgeInsets.only(bottom: 10),
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            child: Row(
-                              children: [
-                                Expanded(
-                                  child: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Text(
-                                        "فلان کار",
-                                        style: TextStyle(
-                                            fontSize: 16,
-                                            fontWeight: FontWeight.bold),
-                                      ),
-                                      Text(
-                                        "توضیحات فلان کار",
-                                        style:
-                                            TextStyle(color: Colors.grey[600]),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                                Icon(
-                                  Icons.delete,
-                                  color: Colors.red,
-                                  size: 20,
-                                )
-                              ],
-                            ),
-                          );
-                        },
+                        itemBuilder: (BuildContext context, int index) =>
+                            TaskItem(context: context,index: index),
                       ),
                     )
                   ],
                 ),
-                Positioned(
-                  bottom: 10,
-                  left: 10,
-                  child: GestureDetector(
-                    onTap: () => Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => TaskScreen())),
-                    child: Container(
-                      child: Icon(
-                        Icons.add,
-                        color: Colors.white,
-                      ),
-                      height: 50,
-                      width: 50,
-                      decoration: BoxDecoration(
-                          color: Colors.red,
-                          borderRadius: BorderRadius.circular(20)),
-                    ),
-                  ),
-                )
+                AddTaskButton()
               ],
             ),
           ),
